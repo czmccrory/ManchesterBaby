07017e326dc433c49fc1bb92f6dcb4645d26b797
fpavlica <33609416+fpavlica@users.noreply.github.com>
2018-11-14 23:43:07 +0000

    now compiles, reads from file, runs-ish
    still missing implementations of each instruction, only printing theie names for now.
    have a quick look through it and please download it before working on it so that you don't overwrite it with your old files again

--- line.h
+++ line.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 
 using namespace std;
 const static int SIZE = 32;
@@ -9,8 +10,10 @@ private:
 public:
 	void print();
 	vector<bool> * getInstruction(); //get the opcode bools from the appropriate places //maybe not int
-	vector<bool> * getAddress(); //get the address bools from approptiate places in the array //maybe not int
+	vector<bool> * getOperand(); //get the address bools from approptiate places in the array //maybe not int
 
+	void set(string);
 	Line();
+	Line(string);
 	~Line();
 };

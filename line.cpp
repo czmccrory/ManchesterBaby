0933fa54ff5ecf31bf5f6b2055b56a9dec4a65fa
fpavlica <33609416+fpavlica@users.noreply.github.com>
2018-11-17 19:41:56 +0000

    everything works now but not very pretty

--- line.cpp
+++ line.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
 #include "line.h"
 
+
+/*
+static void printVector2(vector<bool> * v) {
+	vector<bool>::iterator it;
+	for (it=v->begin(); it != v->end(); ++it) {
+		cout << *it << " " <<flush;
+	}
+	cout <<endl;
+	
+}
+*/
+/*
+  converts an array of booleans (1's and 0's) to a decimal number (little-endian)
+*/
+int binToDec(vector<bool> * bin)
+{
+	bool negative = bin->back(); //1/true means negative, 0/false means positive
+  int length = bin->size();
+  int decimal = 0;
+
+  for (int i = 0; i < length; i++)
+  {
+    if (bin->at(i) == 1) //add value of 2^(length - i - 1)(value of its position in the binary number)
+    {
+      decimal += (static_cast<int>(pow(2, i)));
+    }
+  }
+
+  if (negative) {
+  	return 0-decimal;
+  } else {
+	return decimal;
+  }
+
+}
+
+int uBinToDec(vector<bool> *bin) {
+	vector<bool> temp = *bin;
+	temp.push_back(0);
+	return binToDec(&temp);
+}
+
+
+/*
+  Converts a decimal number to a vector<bool>(32) in little-endian and returns the pointer
+*/
+vector<bool>* decToBin(int dec, int numOfBits) 
+{	
+  vector<bool>* bin = new vector<bool>(numOfBits, 0); // vector to hold binary value (in this case size 8 for rule.)
+
+  const unsigned long long maxDec = pow(2,numOfBits)-1; //we are using operandWidth-bit numbers
+   //with the last bit as the sign
+  bool negative;
+  if (dec < 0) {
+  	negative = true;
+  } else {
+  	negative = false;
+  }
+  for (size_t i=maxDec; dec>0; i--)
+  {
+    // take the remainder of the dec / 2 and save it to the vector
+    // this value will always be 1 or 0
+    (*bin).at(maxDec-i) = dec%2;
+    // update dec
+    dec /= 2;
+  }
+  bin->at(numOfBits-1) = negative; //setting the 13th bit
+
+  return bin;
+
+}
+
+vector<bool> * decToUBin(int udec, int numOfBits) {
+	vector<bool> *bin = decToBin(udec, numOfBits +1);
+	bin->pop_back();
+	return bin;
+}
 
 
 //this should not be here
@@ -11,12 +89,16 @@ void printVector(vector<bool> * v) {
 	for (it=v->begin(); it != v->end(); ++it) {
 		cout << *it << "" <<flush;
 	}
-	cout <<endl;
+	cout <<flush;
 	
+}
+void Line::printNoEndl(){
+	printVector(line);
 }
 
 void Line::print(){
 	printVector(line);
+	cout <<endl;
 }
 
 Line::Line() {
@@ -32,7 +114,7 @@ Line::~Line() {
 	//delete line;
 }
 
-vector<bool> * Line::get(){
+vector<bool> * Line::getVector(){
 	return line;
 }
 
@@ -51,6 +133,30 @@ void Line::setOperand(vector<bool> *op) {
 	}
 }
 
+void Line::setVector(vector<bool> *newVector) {
+	this->line = newVector;
+}
+
+int Line::getDecInstruction(){
+	return binToDec(getInstruction());
+}
+int Line::getDecOperand() {
+	return binToDec(getOperand());
+}
+int Line::getDecVector() {
+	return uBinToDec(getVector());
+}
+void Line::setDecOperand(int decOperand) {
+	setOperand(decToUBin(decOperand));
+
+}
+void Line::setDecVector(int decNumber){
+	setVector(decToBin(decNumber));
+}
+
+
+
+
 void Line::set(string s) {
 	//cout << "trying to set(string) " << endl;
 	//delete line;
@@ -58,17 +164,14 @@ void Line::set(string s) {
 	for (char &c : s) {
 		if (c == '0') {
 			line->push_back(0);
-		} else { //anything that's not 0
+		} else if (c == '1'){ //anything that's not 0
 			line->push_back(1);
 		}
 		//cout << c;
+		//cout << static_cast<int>(c);
 	}
+
 	//cout <<endl;// << "all set. " <<endl;
-	/*
-	for (size_t i = 0; i < s.size(); i++) {
-		line->push_back(s);
-	}
-	*/
 }
 
 /*
--- makefile
+++ makefile
@@ -1,3 +1,11 @@
+CC = g++
+CFLAGS = -Wextra -Wall -pedantic
 
-%: %.cpp
-	g++ -std=c++14 -Wall -Wextra -pedantic $< -o $@
+all: baby.o line.o
+	$(CC) -std=c++14 $(CFLAGS) -o baby baby.o line.o
+	
+baby.o: baby.cpp baby.h line.h
+	$(CC) $(CFLAGS) -c baby.cpp
+
+line.o: line.cpp line.h
+	$(CC) $(CFLAGS) -c line.cpp
